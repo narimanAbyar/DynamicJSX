2fc6bbf0a67965c51c1df5fe80fd61b0e48014e8
narimanAbyar <nariman.abyar11@gmail.com>
2022-02-28 18:59:44 +0330

    finish exercise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
+import React from 'react';
 import './App.css';
+import Person from './person';
 
 function App() {
   return (
     <div>
-      <p>i am person</p>
-      <p>Age:i am x years old</p>
-      
+     <Person fname="nariman" lname="abyar" age="25"/>
+     <Person fname="amir" lname="askari" age="22"/>
+     <Person fname="shayan" lname="ashoori" age="23">sene tabiei</Person>
     </div>
   );
 }
